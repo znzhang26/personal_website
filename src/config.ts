c8452b98371172f9c1b12fa4702124b7e93ed065
Zining Zhang <110920723+znzhang26@users.noreply.github.com>
2024-01-03 19:21:13 -0500

    deploy setting

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Zining Zhang';
-export const SITE_DESCRIPTION = 'Zining Zhang Personal Website';
+export const SITE_DESCRIPTION = 'Zining Zhang Portfolio';
