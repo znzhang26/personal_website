d65ce368ce7af7f2c97fdbd2e7ae60139fe17086
Zining Zhang <110920723+znzhang26@users.noreply.github.com>
2024-01-03 19:18:01 -0500

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://znzhang26.github.io',
+  site: 'https://znzhang.com',
   integrations: [mdx(), sitemap(), tailwind(), image(
     {
       serviceEntryPoint: '@astrojs/image/sharp',
